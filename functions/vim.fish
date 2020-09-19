b906e6a5e3d2b23f5d2d147f81bb08ac9fc4cb55
InnovativeInventor <theinnovativeinventor@gmail.com>
2020-09-19 19:53:29 -0400

    Removed vim alias overload

--- functions/vim.fish
+++
@@ -1,9 +0,0 @@
-function
-    switch (uname)
-        case Linux 
-            nvim $argv
-        case Darwin
-            mvim --servername vim -v $argv
-        case '*'
-            echo "OS not supported"
-end
